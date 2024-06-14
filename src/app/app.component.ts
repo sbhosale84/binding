f8be0743cbf2958ad22a4d8203f3f16d56689fef
sbhosale <sbhosale@osidigital.com>
2024-06-14 16:29:48 +0530

    all binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<img [src]="imageUrl" />
+    <button [attr.aria-label]="ariaLabel">Click me</button>
+    <div [class.active]="isActive">class binding</div>
+    <div [style.color]="color">style binding</div>
+    <button (click)="clickMe()">event binding</button>
+
+    <input [(ngModel)]="name" />
+    <p>Hello {{ name }}</p> `,
+
+  styles: [
+    `
+      .active {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'binding';
+  ariaLabel = 'Submit';
+  isActive = true;
+  color = 'blue';
+  imageUrl = 'https://example.com/image.jpg';
+  name = 'soham';
+  clickMe() {
+    alert('button clicked');
+  }
 }
