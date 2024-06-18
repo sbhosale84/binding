d14e89afb2dca55d530d12db331baf3baddb8cc9
sbhosale <sbhosale@osidigital.com>
2024-06-18 16:33:22 +0530

    all binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<img [src]="imageUrl" />
+  template: `
+    <img [src]="imageUrl" />
     <button [attr.aria-label]="ariaLabel">Click me</button>
     <div [class.active]="isActive">class binding</div>
     <div [style.color]="color">style binding</div>
     <button (click)="toggleColor()">event binding</button>
-    <div>
-      <input [(ngModel)]="name" />
 
-      <p>Hello {{ name }}</p>
-    </div> `,
-
+    <input [(ngModel)]="name" />
+    <p>Hello {{ name }}</p>
+  `,
   styles: [
     `
       .active {
@@ -28,11 +27,9 @@ export class AppComponent {
   color = 'blue';
   imageUrl = 'https://example.com/image.jpg';
   name = 'soham';
-  clickMe() {
-    alert('button clicked');
-  }
+
   toggleColor() {
     this.isActive = !this.isActive;
-    this.color = true ? 'res' : 'blue';
+    this.color = this.isActive ? 'red' : 'blue';
   }
 }
